83d2ff2911ded78825ca9480587be5b27335d65f
Vaibhav-DS1 <vaibhavkubde.ds@gmail.com>
2025-08-03 19:57:50 +0530

    Fix: expose correct port for Render

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template,jsonify,request
 import config
+import os
+
 from project_app.utils import MedicalInsurance
 
 app = Flask(__name__)
@@ -29,3 +31,5 @@ def get_insurance_charges():
     return jsonify({'Result':f"Predicted Medical Insurance Charges are: {charges}"})
 
 app.run(port = config.PORT_NUMBER,debug = 'False')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
